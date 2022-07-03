10ec015ca6854061ae1c198b8188586cd83e13f7
gaofubao <fubao_gao@163.com>
2022-07-03 14:32:17 +0800

    feat(kubernetes): 增加informer示例

---
+++ kubernetes/client-go/informer/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	// 加载配置文件
+	config, err := clientcmd.BuildConfigFromFlags("", "config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	// 初始化客户端
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// 实例化informer
+	sharedInformers := informers.NewSharedInformerFactory(clientSet, time.Minute)
+	informer := sharedInformers.Core().V1().Pods().Informer()
+	// 添加回调方法，当其他组件使用informer机制时触发该回调方法
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			mObj := obj.(v1.Object)
+			log.Printf("new pod added to store: %s", mObj.GetName())
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			oObj := oldObj.(v1.Object)
+			nObj := newObj.(v1.Object)
+			log.Printf("%s pod updated to %s", oObj.GetName(), nObj.GetName())
+		},
+		DeleteFunc: func(obj interface{}) {
+			mObj := obj.(v1.Object)
+			log.Printf("pod deleted from store: %s", mObj.GetName())
+		},
+	})
+
+	// 开始监听
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	informer.Run(stopCh)
+}
