4cee767bb1eec09fee07fa5c1e3dfabdf4c65ce4
gaofubao <fubao_gao@163.com>
2022-07-06 08:35:32 +0800

    feat(algorithm): 46. 全排列

---
+++ algorithm/leetcode/back-tracing/46-permutations/main.go
@@ -0,0 +1,43 @@
+package main
+
+/*
+46. Permutations
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+*/
+
+func permute(nums []int) [][]int {
+	var (
+		res       [][]int
+		track     []int        // 路径
+		used      map[int]bool // 标记路径中的元素是否被使用过
+		backtrack func()
+	)
+
+	used = make(map[int]bool)
+	backtrack = func() {
+		// 结束条件
+		if len(track) == len(nums) {
+			temp := make([]int, len(track))
+			copy(temp, track)
+			res = append(res, temp)
+			return
+		}
+
+		for _, num := range nums {
+			if used[num] {
+				continue
+			}
+			// 做选择
+			used[num] = true
+			track = append(track, num)
+			// 进入下一层决策树
+			backtrack()
+			// 撤销选择
+			used[num] = false
+			track = track[:len(track)-1]
+		}
+	}
+
+	backtrack()
+	return res
+}
