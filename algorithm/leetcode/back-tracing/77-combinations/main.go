b924522dd4a6b5169097d47ae2f5497ac60313c5
gaofubao <fubao_gao@163.com>
2022-07-13 09:16:20 +0800

    feat(algorithm): 77. 组合

--- algorithm/leetcode/back-tracing/77-combinations/main.go
+++ algorithm/leetcode/back-tracing/77-combinations/main.go
@@ -7,5 +7,28 @@ You may return the answer in any order.
 */
 
 func combine(n int, k int) [][]int {
-	return nil
+	var (
+		res       [][]int
+		track     []int
+		backtrack func(start int)
+	)
+
+	backtrack = func(start int) {
+		// 前置位置，只要第k层的节点值
+		if len(track) == k {
+			tmp := make([]int, k)
+			copy(tmp, track)
+			res = append(res, tmp)
+			return
+		}
+
+		for i := start; i <= n; i++ {
+			track = append(track, i)
+			backtrack(i + 1)
+			track = track[:len(track)-1]
+		}
+	}
+
+	backtrack(1)
+	return res
 }
