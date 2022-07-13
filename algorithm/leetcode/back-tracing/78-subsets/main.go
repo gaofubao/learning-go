86cb31c8236314d967c4e28ab73bff2185493c4f
gaofubao <fubao_gao@163.com>
2022-07-13 09:15:06 +0800

    feat(algorithm): 78. 子集

--- algorithm/leetcode/back-tracing/78-subsets/main.go
+++ algorithm/leetcode/back-tracing/78-subsets/main.go
@@ -7,5 +7,28 @@ The solution set must not contain duplicate subsets. Return the solution in any 
 */
 
 func subsets(nums []int) [][]int {
-	return nil
+	var (
+		res       [][]int
+		track     []int
+		backtrack func(start int)
+	)
+
+	backtrack = func(start int) {
+		// 前序位置
+		tmp := make([]int, len(track))
+		copy(tmp, track)
+		res = append(res, tmp)
+
+		for i := start; i < len(nums); i++ {
+			// 做选择
+			track = append(track, nums[i])
+			// 递归，进入下一层决策数，start 参数控制数的遍历，避免产生重复的子集
+			backtrack(i + 1)
+			// 撤销选择
+			track = track[:len(track)-1]
+		}
+	}
+
+	backtrack(0)
+	return res
 }
